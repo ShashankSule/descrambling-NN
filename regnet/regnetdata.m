8e8c75c5b0846e272a66467d0dd5fbbed50e4f40
Shashank Sule <shashanksule8@gmail.com>
2022-06-10 18:33:32 -0400

    Added experiments for x_randn

--- regnet/regnetdata.m
+++ regnet/regnetdata.m
@@ -1,8 +1,8 @@
 %% generate data for the SNR 900 lambda neg 4 regnet
-net = load(strcat(pwd(), '/matdata/Week12_SNR900_lambdaneg4_NDNDvNDB_NN_Week12_BPE_minMetric2.mat'));
+net = load(strcat(pwd(), '/matdata/Week12_SNR10000_lambdaneg4_NDNDvNDB_NN_Week12_NDBPE_minMetric2.mat'));
 data = load(strcat(pwd(), '/_results/_dataSets/_snr900/Week12_SNR900_lambdaneg4_NDNDvNDB_ValidationData.mat'));
-data = data.data(:,1:64); 
-regnet = regnetconverter(net,64); 
+data = data.data(:,1:128); 
+regnet = regnetconverter(net,128); 
 %% done! now run the ton of descramblers and vomit some data out!
 descramblers = {}; 
 for i=1:4
