e4c0f770b8fe78fbe42fdc5139a41e161497cf4a
Shashank Sule <shashanksule8@gmail.com>
2022-06-23 14:36:32 +0530

    SVD's of weights
    visualizing right singular vectors of weights to isolate difference between NDND and NDB

--- regnet/regnetdata.m
+++ regnet/regnetdata.m
@@ -9,4 +9,37 @@ for i=1:4
     L = 2*i;
     descramblers{i} = regnetdescrambler(regnet, data, L, true);
 end
+%% analyze two copies of trained nets 
+ndb = load(strcat(pwd(), '/matdata/Week12_SNR10000_lambdaneg4_NDNDvNDB_NN_Week12_NDBPE_minMetric2.mat'));
+ndnd = load(strcat(pwd(), '/matdata/Week9_SNR10000_lambdaneg1_NDNDvNDB_NN_Week8_NDND_NoDiscardPE_minMetric (3)2.mat'));
+%% compute singular values of each weight matrix
+layernames = fieldnames(ndb); 
+ndbsvd = struct();
+ndndsvd = struct(); 
+%% compute svd's
+for i = 1:5
+    index = 2*i - 1;
+    [undb,sigmandb,vndb] = svd(ndb.(layernames{index})); 
+    [undnd, sigmandnd, vndnd] = svd(ndnd.(layernames{index})); 
+    ndbsvd(i).U = undb; 
+    ndbsvd(i).Sigma = sigmandb; 
+    ndbsvd(i).V = vndb;
+    ndndsvd(i).U = undnd; 
+    ndndsvd(i).Sigma = sigmandnd; 
+    ndndsvd(i).V = vndnd; 
+end
 
+%% plot them svd's! 
+for i=1:5
+    figure(i)
+    tiledlayout(1,2); 
+    nexttile 
+    plot(diag(ndbsvd(i).Sigma), 'bo-'); 
+    hold on; 
+    plot(diag(ndndsvd(i).Sigma), 'ro-');
+    xlabel("Index", 'FontSize', 20); 
+    ylabel("Singular value", 'FontSize', 20); 
+    layername = strcat(num2str(i), "th layer"); 
+    title(strcat("Singular values:", layername), 'FontSize', 20); 
+    nexttile
+    plot(ndbsvd(i).V(:,1:2))
