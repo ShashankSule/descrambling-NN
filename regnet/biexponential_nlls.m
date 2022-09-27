eb00f7f99edcd94837308b2c4b39dd79635308f0
Shashank Sule <shashanksule8@gmail.com>
2022-09-27 14:03:08 -0400

    got figures for paper
    interpolations of singular vectors

--- regnet/biexponential_nlls.m
+++ regnet/biexponential_nlls.m
@@ -1,37 +1,68 @@
 %% design least squares problems to find exponential interpolants
-
+SNR_vec = [1, 900, 10000]; 
+reg_vec = [true, false];
 %% set up network 
-net = load(strcat(pwd(), '/matdata/Week9_SNR10000_lambdaneg1_NDNDvNDB_NN_Week8_NDND_NoDiscardPE_minMetric (2)2.mat'));
-W = net.inputMapweight; 
-[~, ~, V]=svd(W); 
-
-%% get data and dimensions
-SNR = 100; 
-n = 1;
-data = V(:,n); 
-N = max(size(data));
-k = 64; % point at which we split the data
-v_nd = data(1:k); % noisy part 
-v_b = data(k+1:end); % smooth part 
-time_end = 1.6*500;
-t = linspace(time_end/64,time_end,64)'; % get time steps 
-
-%% solve for the interpolants
-
-[i_nd, sol_nd] = find_exp(v_nd, t, 0.0); % get nd interpolant 
-[i_b, sol_b] = find_exp(-v_b, t, 0.0); % get b interpolant 
-
-%% plot the interpolants together 
-plot(data, 'o', 'MarkerSize', 10, 'MarkerFaceColor','blue'); 
-hold on; 
-plot(1:64, i_nd, 'r-', 'LineWidth', 3);
-hold on; 
-plot(65:128, i_nd, 'r-', 'LineWidth', 3);
-title(strcat('SNR: ', num2str(SNR), ', $n = ', num2str(n), '$'), 'FontSize', 20, 'Interpreter','latex')
-set(gca, 'FontSize', 20);
-leg = legend({'Right Singular Vector', 'Bi-exponential Fit'}); 
-set(leg, 'Interpreter', 'latex')
-
+for SNR_index = 1:3 
+    for reg_index = 1:2
+        SNR = SNR_vec(SNR_index); 
+        reg = reg_vec(reg_index); 
+        if reg
+            datastring = 'lambdaneg4_NDB';
+            titlestring = '(ND, Reg)';
+        else
+            datastring = 'lambdaneg1_NDND';
+            titlestring = '(ND, ND)';
+        end
+        
+        filestring = strcat('SNR', num2str(SNR), '_', datastring, '_', 'PE_minMetric.mat');
+        net = load(strcat(pwd(), '/matdata/', filestring));
+        W = net.inputMapweight; 
+        [~, Sigma, V]=svd(W); 
+        %% get data and dimensions
+        for n=1:3
+            % n=3;
+            data = V(:,n); 
+            N = max(size(data));
+            k = 64; % point at which we split the data
+            v_nd = data(1:k); % noisy part 
+            v_b = data(k+1:end); % smooth part 
+            time_end = 1.6*500;
+            t = linspace(time_end/64,time_end,64)'; % get time steps 
+            
+            %% solve for the interpolants
+            
+            [i_nd, sol_nd] = find_exp(v_nd, t, 0.0); % get nd interpolant 
+            if reg
+                [i_b, sol_b] = find_exp(-v_b, t, 0.0); % get b interpolant 
+                i_b = -i_b; % flip sign
+            else
+                i_b = i_nd; 
+            end
+            
+            %% plot the interpolants together 
+            figure;
+            % fiddle with axis limits 
+            ymax = max([data; i_nd; i_b]); 
+            ymin = min([data; i_nd; i_b]);
+            ymax = ymax + 0.05*abs(ymax); ymin = ymin - 0.3*abs(ymin); 
+            plot(data, 'o', 'MarkerSize', 10, 'MarkerFaceColor','blue'); 
+            hold on; 
+            plot(1:64, i_nd, 'r-', 'LineWidth', 3);
+            hold on; 
+            plot(65:128, i_b, 'r-', 'LineWidth', 3);
+            title(strcat(titlestring, ' SNR: ', num2str(SNR), ', $n = ', num2str(n), '$'), 'FontSize', 20, 'Interpreter','latex')
+            set(gca, 'FontSize', 20);
+            %ylim([ymin, ymax]);
+            xlabel 'Index'
+            leg = legend({'Right Singular Vector', 'Bi-exponential Fit'}, 'Location','best'); 
+            set(leg, 'Interpreter', 'latex')
+            saveas(gcf, strcat(pwd(), '/pngdata/sept27/', ...
+                   num2str(SNR), '_', num2str(n), '_', datastring, '.png'))
+            
+        end
+        % close all
+    end
+end
 %% helpers 
 function [y, sol] = find_exp(x,t, lambda)
 
@@ -52,8 +83,8 @@ tr = min(x);
 x = x - tr; 
 obj = mean((f-x).^2) + f_reg;
 lsqproblem = optimproblem("Objective",obj);
-x0.C = [0.18*0.7 + 1e-1*randn(1), 0.18*0.3 + 1e-1*randn(1)];
-x0.T = [0.0055 + 1e-3*randn(1), 0.05 + 1e-3*randn(1)]; 
+x0.C = [0.18*0.7 + 0.0*randn(1), 0.18*0.3 + 0.0*randn(1)];
+x0.T = [0.0055 + 0.0*randn(1), 0.05 + 0.0*randn(1)]; 
 x0.D = -min(x);
 
 %% now solve 
