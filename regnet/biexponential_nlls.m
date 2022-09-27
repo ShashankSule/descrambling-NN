ba42dac2b4337f706b3901904a21b3c0aa5794a5
Shashank Sule <shashanksule8@gmail.com>
2022-09-26 20:21:53 -0400

    NLLS completed
    Added a regularization term to keep solutions simple

--- regnet/biexponential_nlls.m
+++ regnet/biexponential_nlls.m
@@ -1,13 +1,13 @@
 %% design least squares problems to find exponential interpolants
 
 %% set up network 
-net = load(strcat(pwd(), '/matdata/Week12_SNR900_lambdaneg1_NDNDvNDB_NN_Week12_NDNDPE_minMetric (1)2.mat'));
+net = load(strcat(pwd(), '/matdata/Week9_SNR10000_lambdaneg1_NDNDvNDB_NN_Week8_NDND_NoDiscardPE_minMetric (2)2.mat'));
 W = net.inputMapweight; 
-[U, Sigma, V]=svd(W); 
+[~, ~, V]=svd(W); 
 
 %% get data and dimensions
-SNR = 900; 
-n = 3;
+SNR = 100; 
+n = 1;
 data = V(:,n); 
 N = max(size(data));
 k = 64; % point at which we split the data
@@ -18,20 +18,22 @@ t = linspace(time_end/64,time_end,64)'; % get time steps
 
 %% solve for the interpolants
 
-[i_nd, sol_nd] = find_exp(v_nd, t); % get nd interpolant 
-[i_b, sol_b] = find_exp(-v_b, t); % get b interpolant 
+[i_nd, sol_nd] = find_exp(v_nd, t, 0.0); % get nd interpolant 
+[i_b, sol_b] = find_exp(-v_b, t, 0.0); % get b interpolant 
 
 %% plot the interpolants together 
 plot(data, 'o', 'MarkerSize', 10, 'MarkerFaceColor','blue'); 
 hold on; 
-plot([i_nd; -i_b], 'r-', 'LineWidth', 3)
+plot(1:64, i_nd, 'r-', 'LineWidth', 3);
+hold on; 
+plot(65:128, i_nd, 'r-', 'LineWidth', 3);
 title(strcat('SNR: ', num2str(SNR), ', $n = ', num2str(n), '$'), 'FontSize', 20, 'Interpreter','latex')
 set(gca, 'FontSize', 20);
 leg = legend({'Right Singular Vector', 'Bi-exponential Fit'}); 
 set(leg, 'Interpreter', 'latex')
 
 %% helpers 
-function [y, sol] = find_exp(x,t)
+function [y, sol] = find_exp(x,t, lambda)
 
 %find a continuous interpolant C_1*exp(-T_1x) + C_2*exp(-T_2x) + D to x 
 
@@ -44,12 +46,14 @@ C = optimvar('C', 2);
 T = optimvar('T', 2);
 D = optimvar('D', 1);
 f = C(1)*exp(-T(1)*t) - C(2)*exp(-T(2)*t) + D; 
+% lambda = 0.05; 
+f_reg = lambda*(sum(C.^2) + sum(T.^2) + sum(D.^2));
 tr = min(x); 
 x = x - tr; 
-obj = mean((f-x).^2);
+obj = mean((f-x).^2) + f_reg;
 lsqproblem = optimproblem("Objective",obj);
-x0.C = [0.18*0.7, 0.18*0.3];
-x0.T = [0.0055, 0.05]; 
+x0.C = [0.18*0.7 + 1e-1*randn(1), 0.18*0.3 + 1e-1*randn(1)];
+x0.T = [0.0055 + 1e-3*randn(1), 0.05 + 1e-3*randn(1)]; 
 x0.D = -min(x);
 
 %% now solve 
---
+++ regnet/nllsnotes.m
@@ -0,0 +1,17 @@
+% for SNR 900 (ND, B) net 
+% 1st sing vector parameters: 
+% 1. Noisy: expcurve(t, 0.18, -0.0055, -0.05, 0.7, -0.3, min(v_nd));
+% 2. Reg: expcurve(t, 0.18, -0.0055, -0.05, 0.7, -0.3, min(v_nd));
+%
+% 2nd sing vector parameters: 
+% 1. Noisy: expcurve(t, 0.18, -0.0055, -0.05, 0.7, -0.3, min(v_nd));
+% 2. Reg: expcurve(t, 0.18, -0.0055, -0.05, 0.7, -0.3, min(v_nd)); 
+%    For this case, make sure to flip the data: solve  find_exp(-v_b, t)
+%
+% 3rd sing vector parameters: 
+% 1. Noisy: expcurve(t, 0.18, -0.0055, -0.05, 0.7, -0.3, min(v_nd));
+% 2. Reg: expcurve(t, 0.18, -0.0055, -0.05, 0.7, -0.3, min(v_nd)); 
+%    For this case, make sure to flip the data: solve  find_exp(-v_b, t)
+%
+%
+%
